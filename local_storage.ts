b6e7c77eefac94dcf3c7e5275b969cdfc40d7177
Henrique <hv5088@gmail.com>
2023-08-30 11:18:18 -0300

    temp

--- local_storage.ts
+++ local_storage.ts
@@ -5,12 +5,9 @@ Page: https://sites.google.com/view/henriqueviana
 cel: +55 (41) 99999-4664
 */
 
-import { sqlite } from "./deps.ts";
-
 export class LocalStorage {
   private path: string | undefined;
-  private database: sqlite.DB;
-  private tableName: string;
+  private database: Deno.Kv;
   private timeout: number = 0;
   private onDeletedByExpiration:
     | ((data: any) => void | Promise<void>)
@@ -18,31 +15,14 @@ export class LocalStorage {
   private slidingExpiration: number = 0;
   private absoluteExpiration: number = 0;
   constructor(
-    path: string = "./fast_storage.sqlite",
-    tableName: string = "LOCAL_STORAGE",
+    path: string = "./fast_storage",
   ) {
     this.path = path;
-    this.tableName = tableName;
-    this.database = new sqlite.DB(this.path);
-    this.database.query(
-      `CREATE TABLE IF NOT EXISTS ${this.tableName} (key STRING UNIQUE PRIMARY KEY, value TEXT);`,
-    );
-    this.database.query(
-      `CREATE TABLE IF NOT EXISTS ${this.tableName}_timestamp (ref STRING UNIQUE NOT NULL, created INTEGER, updated INTEGER,
-        CONSTRAINT fk_ref
-        FOREIGN KEY (ref)
-        REFERENCES ${this.tableName} (key)
-        ON DELETE CASCADE);
-      `,
-    );
-    this.database.query(
-      `CREATE INDEX IF NOT EXISTS created_idx
-       ON ${this.tableName}_timestamp (created);`,
-    );
-    this.database.query(
-      `CREATE INDEX IF NOT EXISTS updated_idx
-       ON ${this.tableName}_timestamp (updated);`,
-    );
+  }
+  public async init() {
+    if (!this.database) {
+      this.database = await Deno.openKv(this.path);
+    }
   }
 
   public async setCacheConfigs(
@@ -65,25 +45,19 @@ export class LocalStorage {
   }
 
   public async set(
-    key: string,
+    key: string | string[],
     value: any,
     expire: boolean = true,
   ): Promise<void> {
-    if (typeof value !== "string") {
-      value = JSON.stringify(value);
+    await this.init();
+    if (!Array.isArray(key)) {
+      key = key.split(".");
     }
-    this.database.query(
-      `INSERT OR IGNORE INTO ${this.tableName} (key, value) VALUES (?, ?)`,
-      [key, value],
-    );
-    this.database.query(
-      `UPDATE OR IGNORE ${this.tableName} SET value = ? WHERE key = ?`,
-      [value, key],
-    );
+    await this.database.set(key, value);
     await this.#updateTime(key, expire);
   }
 
-  async #updateTime(ref: string, expire: boolean) {
+  async #updateTime(ref: string[], expire: boolean) {
     if (
       expire
     ) {
@@ -93,24 +67,16 @@ export class LocalStorage {
         created = updated;
       }
       try {
-        this.database.query(
-          `INSERT OR IGNORE INTO ${this.tableName}_timestamp (ref, created, updated) VALUES (?, ?, ?)`,
-          [ref, created, updated],
-        );
-        this.database.query(
-          `UPDATE OR IGNORE ${this.tableName}_timestamp SET created = ?, updated = ? WHERE ref = ?`,
-          [created, updated, ref],
-        );
+        await this.database.set([...ref, "created"], created);
+        await this.database.set([...ref, "updated"], updated);
       } catch (e) {
       }
       if (this.timeout === 0) {
         await this.#setValTimeout();
       }
     } else {
-      this.database.query(
-        `DELETE FROM ${this.tableName}_timestamp WHERE ref = ?`,
-        [ref],
-      );
+      await this.database.delete([...ref, "created"]);
+      await this.database.delete([...ref, "updated"]);
     }
   }
 
@@ -170,42 +136,25 @@ export class LocalStorage {
     return [minCreated, minUpdated];
   }
 
-  public async get(key: string) {
-    for (
-      var [value] of this.database.query(
-        `SELECT value FROM ${this.tableName} WHERE key = ?`,
-        [key],
-      )
-    ) {
-      await this.#updateTime(key, await this.expire(key));
-      return this.#parseRes("", value as string, false);
+  public async get(key: string | string[]) {
+    await this.init();
+    if (!Array.isArray(key)) {
+      key = key.split(".");
     }
+    const result = await this.database.get(key);
+    await this.#updateTime(result.key, await this.expire(result.key));
+    return result.value;
   }
-  async #getCreatedDate(ref: string) {
-    for (
-      var [value] of this.database.query(
-        `SELECT created FROM ${this.tableName}_timestamp WHERE ref = ?`,
-        [ref],
-      )
-    ) {
-      const res = parseInt(value as string);
-      if (!isNaN(res)) {
-        return res;
-      }
+  async #getCreatedDate(ref: string[]) {
+    const res = await this.database.set([...ref, "created"]);
+    if (!isNaN(res)) {
+      return res;
     }
     return 0;
   }
-  async expire(ref: string): Promise<boolean> {
-    const expired: string[] = [];
-    for (
-      var [existingRef] of this.database.query(
-        `SELECT ref FROM ${this.tableName}_timestamp WHERE ref = ?;`,
-        [ref],
-      )
-    ) {
-      expired.push(existingRef as string);
-    }
-    return expired.length > 0;
+  async expire(ref: string[]): Promise<boolean> {
+    const res = await this.database.get([...ref, "created"]);
+    return (res !== null);
   }
   async getExpired(): Promise<string[]> {
     const expired: string[] = [];
@@ -235,127 +184,118 @@ export class LocalStorage {
   async #deleteExpired(): Promise<void> {
     const expired: string[] = await this.getExpired();
     const callBackData: any[] = [];
-    var expiredStr: string = "";
     for (const ref of expired) {
       if (this.onDeletedByExpiration) {
-        callBackData.push({ key: ref, data: await this.get(ref) });
+        await this.onDeletedByExpiration({
+          key: ref,
+          data: await this.get(ref),
+        });
       }
-      expiredStr += `'${ref}'` + ",";
-    }
-    if (expired.length > 0) {
-      this.database.query(
-        `DELETE FROM ${this.tableName} WHERE key in (${
-          expiredStr.slice(0, -1)
-        })`,
-      );
-      if (this.onDeletedByExpiration) {
-        for (const data of callBackData) {
-          await this.onDeletedByExpiration(data);
-        }
-      }
+      await this.delete(ref);
     }
     await this.#setValTimeout();
   }
 
-  public async delete(key: string): Promise<void> {
-    this.database.query(
-      `DELETE FROM ${this.tableName} WHERE key = ?`,
-      [key],
-    );
-  }
-  #parseRes(key: string, value: string, getKey: boolean = false): any {
-    var parsedValue: any = undefined;
-    try {
-      //@ts-ignore
-      parsedValue = JSON.parse(value);
-    } catch (e) {
-      parsedValue = value;
+  public async delete(key: string | string[]): Promise<void> {
+    await this.init();
+    if (!Array.isArray(key)) {
+      key = key.split(".");
     }
+    await this.database.delete(key);
+    await this.database.delete([...key, "created"]);
+    await this.database.delete([...key, "updated"]);
+  }
+  #parseRes(key: string[], value: any, getKey: boolean = false): any {
     if (getKey) {
       return {
         key: key,
-        value: parsedValue,
+        value: value,
       };
     } else {
-      return parsedValue;
+      return value;
     }
   }
   public async getNameSpace(
-    keyStartsWith: string,
+    prefix: string | string[],
     getKey: boolean = false,
   ): Promise<any[]> {
+    await this.init();
+    if (!Array.isArray(prefix)) {
+      prefix = prefix.split(".");
+    }
     const res: any[] = [];
-    for (
-      var [key, value] of this.database.query(
-        `SELECT key, value FROM ${this.tableName} WHERE key >= ? AND key < ?`,
-        [keyStartsWith, this.#incrementString(keyStartsWith)],
-      )
-    ) {
-      await this.#updateTime(key, await this.expire(key));
-      res.push(this.#parseRes(key as string, value as string, getKey));
+    const entries = await this.database.list({ prefix: prefix });
+    for await (const entry of entries) {
+      await this.#updateTime(entry.key, await this.expire(entry.key));
+      res.push(this.#parseRes(entry.key, entry.value, getKey));
     }
     return res;
   }
-  public async deleteNameSpace(keyStartsWith: string): Promise<void> {
-    this.database.query(
-      `DELETE FROM ${this.tableName} WHERE key >= ? AND key < ?`,
-      [keyStartsWith, this.#incrementString(keyStartsWith)],
-    );
+  public async deleteNameSpace(prefix: string | string[]): Promise<void> {
+    await this.init();
+    if (!Array.isArray(prefix)) {
+      prefix = prefix.split(".");
+    }
+    const entries = await this.database.list({ prefix: prefix });
+    for await (const entry of entries) {
+      await this.delete(entry.key);
+    }
   }
-  #mountRangeQuery(
-    initial: string,
-    start?: string,
-    end?: string,
-  ): [string, string[]] {
-    var query: [string, string[]] = [
-      initial,
-      [],
-    ];
-    if (start) {
-      query[0] += `key >= ? `;
-      query[1].push(start);
-      if (end) {
-        query[0] += `AND `;
+  #parseListSelectorOptions(
+    prefix?: string | string[],
+    start?: string | string[],
+    end?: string | string[],
+  ): any {
+    const filters: any = {};
+    if (prefix) {
+      if (!Array.isArray(prefix)) {
+        prefix = prefix.split(".");
       }
+      filters["prefix"] = prefix;
+    }
+    if (start) {
+      if (!Array.isArray(start)) {
+        start = start.split(".");
+      }
+      filters["start"] = start;
     }
     if (end) {
-      query[0] += `key <= ? `;
-      query[1].push(end);
+      if (!Array.isArray(end)) {
+        end = end.split(".");
+      }
+      filters["end"] = end;
     }
-    return query;
+    return filters;
   }
-  public async deleteRange(start?: string, end?: string): Promise<void> {
-    var query: [string, string[]] = this.#mountRangeQuery(
-      `DELETE FROM ${this.tableName} WHERE `,
-      start,
-      end,
-    );
-    this.database.query(query[0], query[1]);
+
+  public async deleteRange(
+    prefix?: string | string[],
+    start?: string | string[],
+    end?: string | string[],
+  ): Promise<void> {
+    await this.init();
+    const filters = this.#parseListSelectorOptions(prefix, start, end);
+    const res: any[] = [];
+    const entries = await this.database.list(filters);
+    for await (const entry of entries) {
+      await this.delete(entry.key);
+    }
   }
   public async getRange(
-    start?: string,
-    end?: string,
+    prefix?: string | string[],
+    start?: string | string[],
+    end?: string | string[],
     getKey: boolean = false,
   ): Promise<any[]> {
-    var query: [string, string[]] = this.#mountRangeQuery(
-      `SELECT key, value FROM ${this.tableName} WHERE `,
-      start,
-      end,
-    );
+    await this.init();
+    const filters = this.#parseListSelectorOptions(prefix, start, end);
     const res: any[] = [];
-    for (
-      var [key, value] of this.database.query(query[0], query[1])
-    ) {
-      await this.#updateTime(key, await this.expire(key));
-      res.push(this.#parseRes(key as string, value as string, getKey));
+    const entries = await this.database.list(filters);
+    for await (const entry of entries) {
+      await this.#updateTime(entry.key, await this.expire(entry.key));
+      res.push(this.#parseRes(entry.key, entry.value, getKey));
     }
     return res;
-  }
-  #incrementString(s: string) {
-    return s.replace(/.$/, this.#nextChar(s.slice(-1)));
-  }
-  #nextChar(c: string) {
-    return String.fromCharCode(c.charCodeAt(0) + 1);
   }
 }
 
